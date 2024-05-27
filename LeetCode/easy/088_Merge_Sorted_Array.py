e2cc83c8f790c7c3a480c96432ec0c5e341a608c
Vadali Sai Krishna <saikrishnavadali05@gmail.com>
2024-05-27 22:29:09 +0530

    Update 088_Merge_Sorted_Array.py

--- LeetCode/easy/088_Merge_Sorted_Array.py
+++ LeetCode/easy/088_Merge_Sorted_Array.py
@@ -4,6 +4,7 @@ class Solution:
     def shift_num1_elements(num1, shift_pos):
         for i range()
 
+    # trial 1
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
@@ -23,3 +24,27 @@ class Solution:
                     nums1[i + 1] = nums2[j]
 
         return None
+
+    #trial 2
+    # def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    #         """
+    #         Do not return anything, modify nums1 in-place instead.
+    #         """
+    #         j = 0
+    #         while(1):
+    #             if nums1[i] < nums2[j]:
+    #                 i = i+1
+                
+    #             elif nums1[i] == nums2[j]:
+    #                 nums1[i + 1] = nums2[j]
+    #                 i = i + 1
+    #                 j = j + 1
+    
+    #                 for k, n1 in enumerate(nums1[i:0], start=0):
+    #                     nums1[k + 1]
+    
+    #             elif nums1[i] > nums2[j]:
+    #                 nums1[i + 1] = nums2[j]
+    
+    #         return None
+    
